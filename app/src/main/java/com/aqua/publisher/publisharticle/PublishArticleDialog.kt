df5147048d4e9f453c49e2d53ca0fc836ed2915b
Aqua <huangaqua@icloud.com>
2021-10-12 11:47:22 +0800

    add dialog UI

--- app/src/main/java/com/aqua/publisher/publisharticle/PublishArticleDialog.kt
+++ app/src/main/java/com/aqua/publisher/publisharticle/PublishArticleDialog.kt
@@ -1,16 +1,16 @@
 package com.aqua.publisher.publisharticle
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import com.aqua.publisher.R
-import com.aqua.publisher.databinding.PublishArticleFragmentBinding
+import com.aqua.publisher.databinding.PublishArticleDialogBinding
 
-class PublishArticleFragment : Fragment() {
+class PublishArticleDialog : DialogFragment() {
 
     private val viewModel: PublishArticleViewModel by lazy {
         ViewModelProvider(this).get(PublishArticleViewModel::class.java)
@@ -20,8 +20,8 @@ class PublishArticleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: PublishArticleFragmentBinding =
-            DataBindingUtil.inflate(inflater, R.layout.publish_article_fragment, container,false)
+        val binding: PublishArticleDialogBinding =
+            DataBindingUtil.inflate(inflater, R.layout.publish_article_dialog, container,false)
 
         return binding.root
     }
